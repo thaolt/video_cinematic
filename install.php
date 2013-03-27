93467bf74885538436ded7f1a2e9d41136da1af3
ycicom <thaolt@songphi.com>
2013-03-27 21:15:01 +0700

    Update install.php
    fix missing setting option for the plugin

--- install.php
+++ install.php
@@ -8,3 +8,5 @@ $config->addConfig('video_cinematic', 'preset', 'full');
 $config->addConfig('video_cinematic', 'borderColor', '#FFFFFF');
 $config->addConfig('video_cinematic', 'displayLogo', '1');
 $config->addConfig('video_cinematic', 'logoFile', '');
+
+OW::getPluginManager()->addPluginSettingsRouteName('video_cinematic', 'video_cinematic.admin');
