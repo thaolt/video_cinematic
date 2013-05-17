079419f13d0a6d4332d46296f7596346167042e8
Thao Le <thaolt@songphi.com>
2013-05-17 21:13:22 +0700

    clean up static dir when updating

--- update/10200/update.php
+++ update/10200/update.php
@@ -1,3 +1,15 @@
 <?php
 
+$plugin = OW::getPluginManager()->getPlugin('photo');
+
+$staticDir = OW_DIR_STATIC_PLUGIN . $plugin->getModuleName() . DS;
+
+if ( file_exists($staticDir) ) {
+    UTIL_File::removeDir($staticDir);
+}
+mkdir($staticDir);
+chmod($staticDir, 0777);
+
+UTIL_File::copyDir( $plugin->getStaticDir(), $staticDir );
+
 Updater::getLanguageService()->importPrefixFromZip(dirname(__FILE__).DS.'langs.zip', 'video_cinematic');
